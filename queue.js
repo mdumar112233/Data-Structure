1f2d95ea168284252e8e0e1a6f7a7ab9d85f6615
md umar faruk <mdumar11233>
2021-04-23 15:13:23 +0600

    added new code

--- queue.js
+++ queue.js
@@ -1,3 +1,30 @@
 // First in First out
 // FIFO
+class Queue{
+    constructor() {
+        this.queue = [];
+    }
+    enqueue(item){
+        this.queue.push(item)
+    }
+    dequeue(){
+        if(this.queue.length){
+            return this.queue.shift();
+        }
+    }
+}
+
+
+const myQueue = new Queue();
+myQueue.enqueue('rofik');
+myQueue.enqueue('sofik');
+myQueue.enqueue('ahmed');
+myQueue.enqueue('josim');
+console.log(myQueue.queue);
+myQueue.dequeue();
+console.log(myQueue.queue);
+
+
+
+
 
